f2ec1fa75ceda36d15f44f7cf2ec5d54f7b7a777
tania_esd <tania.carro@esdmadrid.es>
2026-01-15 13:50:47 +0100

    actualizacion

---
+++ assets/javascript/nav.js
@@ -0,0 +1,8 @@
+function openMenu() {
+  var x = document.getElementById("mySections");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
